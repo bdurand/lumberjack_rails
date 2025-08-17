d6c13bdfab9c773fbec92693b01d5a24fdce8bc1
Brian Durand <bbdurand@gmail.com>
2025-08-17 14:25:32 -0700

    remove rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,7 +36,6 @@ require_relative "../lib/lumberjack_rails"
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
-  config.default_formatter = "doc" if config.files_to_run.one?
   config.order = :random
   Kernel.srand config.seed
 end
