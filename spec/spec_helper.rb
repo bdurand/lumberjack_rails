9ad608caad9c80c1b28fe13e370a648c7852b564
Brian Durand <bbdurand@gmail.com>
2025-08-17 14:26:24 -0700

    restore spec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,7 @@ require_relative "../lib/lumberjack_rails"
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
+  config.default_formatter = "doc" if config.files_to_run.one?
   config.order = :random
   Kernel.srand config.seed
 end
