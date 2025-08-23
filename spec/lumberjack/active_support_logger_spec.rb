5122ab277319c09bc15ca8079a26e72c701085ca
Brian Durand <bbdurand@gmail.com>
2025-08-23 10:30:28 -0700

    Add tests to assert compatibility with ActiveSupport::Logger

---
+++ spec/lumberjack/active_support_logger_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+RSpec.describe "ActiveSupport::Logger compatibility" do
+  describe "logger_outputs_to?" do
+    it "detects the underlying stream from a Writer device" do
+      stream = StringIO.new
+      device = Lumberjack::Device::Writer.new(stream)
+      logger = Lumberjack::Logger.new(device)
+      expect(ActiveSupport::Logger.logger_outputs_to?(logger, stream)).to be true
+      expect(ActiveSupport::Logger.logger_outputs_to?(logger, StringIO.new)).to be false
+    end
+
+    it "detects the underlying stream from a LoggerFile device" do
+      stream = StringIO.new
+      device = Lumberjack::Device::LoggerFile.new(stream)
+      logger = Lumberjack::Logger.new(device)
+      expect(ActiveSupport::Logger.logger_outputs_to?(logger, stream)).to be true
+      expect(ActiveSupport::Logger.logger_outputs_to?(logger, StringIO.new)).to be false
+    end
+  end
+end
