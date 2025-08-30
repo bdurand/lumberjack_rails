adbe32a4eb9273d54aa656a3702c9d5b3ef4cb6a
Brian Durand <bbdurand@gmail.com>
2025-08-29 17:34:42 -0700

    update test rails app

--- test_app/app/controllers/application_controller.rb
+++ test_app/app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  around_action :log_duration
+
+  def log_duration
+    start_time = Time.now
+    result = yield
+    duration = Time.now - start_time
+    Rails.logger.tag(duration: duration)
+    result
+  end
 end
---
+++ test_app/app/controllers/test_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class TestController < ApplicationController
+  def index
+  end
+end
